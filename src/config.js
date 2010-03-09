1669caee60bd1013a21da616631dea1932d801c6
bga <bga.email@gmail.com>
2010-03-09 04:24:42 +0300

    corrected rootUrl for correct modules load directly from github

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@ window.$jb={};
 
 $jb.Cfg=
 {
- rootUrl:"/p/jbasis/src/",
+ //rootUrl:"/p/jbasis/src/",
+ rootUrl:"http://github.com/bga/jbasis/raw/master/src/",
  /* rootId_:"jbasis" */
  blankImageUrl:"/p/jbasis/src/_images/spacer.gif"
  //isMultiDocument:false
