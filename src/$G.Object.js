2d45e6a10e05a9f8cd57ccd3e6119fb11c36339c
bga <bga.email@gmail.com>
2010-04-10 20:37:34 +0400

    code readability improved, Object,getPrototypeOf added, opera String.propertyIsEnumerably workaround added, Object._length and Object._isEmpty now use Object.__count__ spidermokey js extention if possable, Object._dump moved to $w.console.js, Object._isPrimitiveType and Object._findIf removed

--- src/$G.Object.js
+++ src/$G.Object.js
@@ -33,36 +33,49 @@
  
   @section DESCRIPTION
   
-  provode 4 functions for for copyng and cloning with or without strict mode
+  make ecma5 Object features available to use
+  add filters
+  and _invert _length _isEmpty
 */
 
 $jb.Loader._scope().
+//_require("$jb/$jb.nav.js").
 _willDeclared("$jb/$G.Object.js").
 _completed(function(){
 
-// check argument is it primitive type (String, Number, Boolean) 
-//
-// @param obj object to check
-// @return true for primitive else false 
-$jb._isPrimitiveType=function(obj)
+if(!Object.getPrototypeOf)
 {
-  if(obj==null)
-    return true;
-  
-  var type=typeof(obj);
-  
-  return type !== "object" && type !== "function";
-};
+  if("__proto__" in {})
+  {
+    Object.getPrototypeOf = function(v)
+    {
+      if(typeof(v) !== "object" || v == null)
+        throw new TypeError();
+      
+      return v.__proto__;
+    };  
+  }
+  else
+  {
+    Object.getPrototypeOf = function(v)
+    {
+      if(typeof(v) !== "object" || v == null)
+        throw new TypeError();
+      
+      return v.constructor.prototype;
+    };
+  }
+}
 
-if(typeof(Object.defineProperties) != "function")
+if(typeof(Object.defineProperties) !== "function")
 {
-  Object.defineProperties=function(to, from)
+  Object.defineProperties = function(to, from)
   {
     //15.2.3.7 If Type(O) is not Object throw a TypeError exception. 
-    if(to==null || typeof(to) !== 'object')
+    if(to == null || typeof(to) !== 'object')
       throw TypeError();
       
-    var type=typeof(from);
+    var type = typeof(from);
     
     //15.2.3.7 Let props be ToObject(Properties). 
     if(type === 'object' || type === 'function')
@@ -70,71 +83,69 @@ if(typeof(Object.defineProperties) != "function")
       for(var i in from)
       {
         if(from.hasOwnProperty(i))
-          to[i]=from[i];
+          to[i] = from[i];
       }
     }
     
-    return this;
+    return to;
   };
 }
 
-if(typeof(Object.create) != "function")
+if(typeof(Object.create) !== "function")
 {
   if("__proto__" in {})
   {
-    Object.create=function(pr,initObj)
+    Object.create = function(pr, initObj)
     {
       if(typeof(pr) !== 'object')
         throw new TypeError();
 
-      var obj=new Object();
+      var obj = new Object();
         
-      obj.__proto__=pr;
+      obj.__proto__ = pr;
       
       if(initObj!=null)
-        Object.defineProperties(obj,initObj)
+        Object.defineProperties(obj, initObj)
       
       return obj;
     };
   }
   else
   {
-    (function()
+    Object.create = function(pr, initObj)
     {
-      var _constructor=function() {};
+      if(typeof(pr) !== 'object')
+        throw new TypeError();
       
-      Object.create=function(pr,initObj)
+      var obj;
+      
+      if(pr !== Object.prototype)
+      {  
+        var _constructor = function(){};
+    
+        _constructor.prototype = pr;
+        
+        obj = new _constructor();
+        obj.constructor = _constructor; // ie not set 'constructor' property if 'prototype' of function replaced
+      }
+      else
       {
-        if(typeof(pr) !== 'object')
-          throw new TypeError();
-        
-        var obj;
-        
-        if(pr !== Object.prototype)
-        {  
-          _constructor.prototype=pr;
-          
-          obj=new _constructor();
-        }
-        else
-        {
-          obj=new Object();
-        }
-        
-        if(initObj!=null)
-          Object.defineProperties(obj,initObj)
+        obj = new Object();
+      }
+      
+      if(initObj != null)
+        Object.defineProperties(obj, initObj)
 
-        return obj;
-      };
-    })();  
+      return obj;
+    };
   }  
 }
 
 if(!("propertyIsEnumerable" in {}))
 {
-  Object.prototype.propertyIsEnumerable=function(name)
+  Object.prototype.propertyIsEnumerable = function(name)
   {
-    if(!(name in this))
+    if(!this.hasOwnProperty(name))
       return false;
       
     var i;
@@ -149,232 +160,250 @@ if(!("propertyIsEnumerable" in {}))
   };
 }
 
-if(!("hasOwnProperty" in {}))
-{
-  Object.prototype.hasOwnProperty=function(name)
+// https://twitter.com/bga_/status/11006228335
+// new String('10').propertyIsEnumerable('0') === false , but key '0' is in enumeration.
+if('10'.propertyIsEnumerable('0') === false && 
+  (function(v)
   {
-    return (name in this) || this.constructor.prototype[name] !== this[name];
-  };
+    for(var i in v)
+    {  
+      if(i === '0')
+        return true;
+    }
+    
+    return false;
+  })('10')
+)
+{
+  (function()
+  {
+    var _oPropertyIsEnumerable = String.prototype.propertyIsEnumerable;
+    var re = new RegExp(/^\d+$/);
+    
+    String.prototype.propertyIsEnumerable = function(name)
+    {
+      if(re.test(name))
+        return true;
+      
+      return _oPropertyIsEnumerable.call(this);
+    };
+  })();
 }
 
-
-if($jb.Filter==null)
-  $jb.Filter={};
-
-Object.prototype._filterExtra=function(i,obj)
+Object.prototype._filterExtra = function(i, obj)
 {
   return obj.hasOwnProperty(i);
 };
-Number.prototype._filterExtra=Object.prototype._filterExtra;
-Boolean.prototype._filterExtra=Object.prototype._filterExtra;
-RegExp.prototype._filterExtra=Object.prototype._filterExtra;
-Date.prototype._filterExtra=Object.prototype._filterExtra;
-Array.prototype._filterExtra=function(i,obj)
+Number.prototype._filterExtra = function(i, obj)
 {
-  return obj.hasOwnProperty(i) && !(+i>=0); // not correct by realy fast!
+  return obj.hasOwnProperty(i);
+};
+Boolean.prototype._filterExtra = function(i, obj)
+{
+  return obj.hasOwnProperty(i);
+};
+RegExp.prototype._filterExtra = function(i, obj)
+{
+  return obj.hasOwnProperty(i);
+};
+Date.prototype._filterExtra = function(i, obj)
+{
+  return obj.hasOwnProperty(i);
+};
+Array.prototype._filterExtra = function(i, obj)
+{
+  return obj.hasOwnProperty(i) && !(+i >= 0); // not correct by realy fast!
 };
 
 if(Object.prototype._filterExtra.propertyIsEnumerable("prototype"))
 {  
-  Function.prototype._filterExtra=function(i,obj)
+  Function.prototype._filterExtra = function(i, obj)
   {
-    return obj.hasOwnProperty(i) && i!=="prototype";
+    return obj.hasOwnProperty(i) && i !== "prototype";
   };
 }
 else
 {
-  Function.prototype._filterExtra=Object.prototype._filterExtra;
+  Function.prototype._filterExtra==function(i, obj)
+  {
+    return obj.hasOwnProperty(i);
+  };
 }
 
-if("1".propertyIsEnumerable("0"))
-  String.prototype._filterExtra=Array.prototype._filterExtra;
+if(Object("1").propertyIsEnumerable("0"))
+{
+  String.prototype._filterExtra = function(i, obj)
+  {
+    return obj.hasOwnProperty(i) && !(+i >= 0); // not correct by realy fast!
+  };
+}
 else
-  String.prototype._filterExtra=Object.prototype._filterExtra;
-
+{
+  String.prototype._filterExtra = function(i, obj)
+  {
+    return obj.hasOwnProperty(i);
+  };
+}
 
 // copy member by member "obj" to result
-Object.prototype._rawCopyFrom=function(src,_filter)
+Object.prototype._rawCopyFrom = function(src, _filter)
 {
-  if(src==null)
+  if(src == null)
     return null;
 
-  if(_filter==null)
-    _filter=src._filterExtra;
+  if(_filter == null)
+    _filter = src._filterExtra;
   
-  var i=null;
+  var i = null;
   
   for(i in src)
   {
-    if(_filter(i,src))
-      this[i]=src[i];
+    if(_filter(i, src))
+      this[i] = src[i];
   }
 
   return this;
 };
-Object.prototype._rawCopyFromAsObject=Object.prototype._rawCopyFrom;
+Object.prototype._rawCopyFromAsObject = Object.prototype._rawCopyFrom;
 
 // invert target key=>value pairs to value=>key and set it to destObj or newly created object
 //
 // @param destObj optional. dest object or newly created object
 // @param _filter optional.  
-Object.prototype._invert=function(destObj,_filter)
+Object.prototype._invert = function(destObj, _filter)
 {
-  var i=null;
-  var im=destObj || {};
+  var i = null;
   
-  if(_filter==null)
-    _filter=this._filterExtra;
+  if(destObj == null)
+    destObj = {};
+  
+  if(_filter == null)
+    _filter = this._filterExtra;
 
   for(i in this)
   {
-    if(_filter(i,this))
-      im[this[i]]=i;
+    if(_filter(i, this))
+      destObj[this[i]] = i;
   }
   
-  return im;
+  return destObj;
 };
-Object.prototype._invertAsObject=Object.prototype._invert;
+Object.prototype._invertAsObject = Object.prototype._invert;
 
-Object.prototype._dumpAsObject=function(_filter)
-{
-  if(_filter==null)
-    _filter=this._filterExtra;
-  
-  var t="{ ";
-  var i=null;
-  
-  for(i in this)
-  {
-    if(_filter(i,this))
-    {
-      t+="\"";
-      t+=i;
-      t+="\" = ";
-      t+=this[i];
-      t+=", ";
-    }
-  }
-  
-  t+="}";
-  
-  return t;
-};
-Object.prototype._dump=Object.prototype._dumpAsObject;
-
-Object.prototype._find=function(value,_filter)
+Object.prototype._find = function(value, _filter)
 {
   if(filter)
-    _filter=this._filterExtra;
+    _filter = this._filterExtra;
 
-  var i=null;
+  var i = null;
   
   for(i in this)
   {
-    if(_filter(i,this) && this[i]===value)
+    if(_filter(i, this) && this[i] === value)
       return i;
   }
   
   return null;
 };
-Object.prototype._findAsObject=Object.prototype._find;
+Object.prototype._findAsObject = Object.prototype._find;
 
-Object.prototype._findIf=function(_pred,_filter)
+if(Object.__count__ != null)
 {
-  if(_pred==null)
-    return null;
-    
-  if(filter)
-    _filter=this._filterExtra;
-
-  var i=null;
-  
-  for(i in this)
-  {
-    if(_filter(i,this) && !_pred(i,this))
-      return i;
-  }
-  
-  return null;
-};
-Object.prototype._findIfAsObject=Object.prototype._findIf;
-
-if(Object.__count__!=null)
-{
-  Object.prototype._filterExtra._length=
-  Number.prototype._filterExtra._length=
-  Boolean.prototype._filterExtra._length=
-  Function.prototype._filterExtra._length=
-  RegExp.prototype._filterExtra._length=
+  Object.prototype._filterExtra._length = 
+  Number.prototype._filterExtra._length = 
+  Boolean.prototype._filterExtra._length = 
+  Function.prototype._filterExtra._length = 
+  RegExp.prototype._filterExtra._length = 
   function()
   {
     return this.__count__;
   };
-  
-  String.prototype._filterExtra._length=function()
+
+  Object.prototype._filterExtra._isEmpty = 
+  Number.prototype._filterExtra._isEmpty = 
+  Boolean.prototype._filterExtra._isEmpty = 
+  Function.prototype._filterExtra._isEmpty = 
+  RegExp.prototype._filterExtra._isEmpty = 
+  function()
   {
-    return this.__count__-this.length;
+    return this.__count__ === 0;
+  };
+  
+  String.prototype._filterExtra._length = function()
+  {
+    return this.__count__ - this.length;
+  };
+  String.prototype._filterExtra._isEmpty = function()
+  {
+    return this.__count__ - this.length === 0;
+  };
+  
+  Array.prototype._filterExtra._length = function()
+  {
+    if(this.__count__ === 0)
+      return 0;
+      
+    if(this.length === 0 && this.__count__ > 0)
+      return this.__count__;
+      
+    return null;
+  };
+  Array.prototype._filterExtra._isEmpty = function()
+  {
+    if(this.__count__ === 0)
+      return true;
+      
+    if(this.length === 0 && this.__count__ > 0)
+      return false;
+      
+    return null;
   };
 }
 
-Object.prototype._length=null;
+Object.prototype._length = null;
 
-Object.prototype._length=function(_filter)
+Object.prototype._length = function(_filter)
 {
-  if(_filter==null)
-    _filter=this._filterExtra;
+  if(_filter == null)
+    _filter = this._filterExtra;
 
-  if(_filter._length!=null)
-    return _filter._length.call(this);
+  var ret;
+  
+  if(_filter.hasOwnProperty('_length') && (ret = _filter._length.call(this)) != null)
+    return ret;
     
-  var i;
-  var n=0;
+  var i, n = 0;
   
   for(i in this)
   {
-    if(_filter(i,this))
+    if(_filter(i, this))
       ++n;
   }
   
   return n;
 };
-Object.prototype._lengthAsObject=Object.prototype._length;
+Object.prototype._lengthAsObject = Object.prototype._length;
 
-Object.prototype._isEmpty=function(_filter)
+Object.prototype._isEmpty = function(_filter)
 {
-  if(_filter==null)
-    _filter=this._filterExtra;
+  if(_filter == null)
+    _filter = this._filterExtra;
 
+  var ret;
+  
+  if(_filter.hasOwnProperty('_isEmpty') && (ret = _filter._isEmpty.call(this)) != null)
+    return ret;
+  
   var i;
   
   for(i in this)
   {
-    if(_filter(i,this))
+    if(_filter(i, this))
       return false;
   }
   
   return true;
 };
-Object.prototype._isEmptyAsObject=Object.prototype._isEmpty;
-
-Object.prototype._each=function(_func,_filter)
-{
-  if(_func==null)
-    return this;
-
-  if(filter)
-    _filter=this._filterExtra;
-
-  var i=null;
-  
-  for(i in this)
-  {
-    if(_filter(i,this))
-      return _func(this[i]);
-  }
-  
-  return this;
-};
-Object.prototype._eachAsObject=Object.prototype._each;
-
+Object.prototype._isEmptyAsObject = Object.prototype._isEmpty;
 
 });
