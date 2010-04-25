2669828d44923b8532611cd91665ff9b5d603284
bga <bga.email@gmail.com>
2010-04-25 18:16:05 +0400

    arguments.lenght > 0 optomization; new Function.prototype._isNative method; _fBind future optimization via native Function.prototype.bind

--- src/$G.Function.js
+++ src/$G.Function.js
@@ -48,7 +48,7 @@ _completed(function(){
 */
 Function.prototype._apply=function(that,args)
 {
-  return (args==null) ? this.call(that) : this.apply(that,args);
+  return (args == null || args.length === 0) ? this.call(that) : this.apply(that, args);
 };
 
 /**
@@ -56,12 +56,12 @@ Function.prototype._apply=function(that,args)
   @param code function code. Optional
   @return header string
 */
-Function.prototype._header=function(code)
+Function.prototype._header = function(code)
 {
-  if(code==null)
-    code=""+this;
+  if(code == null)
+    code = "" + this;
   
-  return code.substring(0,code.indexOf(")")+1);
+  return code.substring(0, code.indexOf(")") + 1);
 };
 
 /**
@@ -69,12 +69,12 @@ Function.prototype._header=function(code)
   @param code function code. Optional
   @return name string
 */
-Function.prototype._name=function(code)
+Function.prototype._name = function(code)
 {
-  if(code==null)
-    code=""+this;
+  if(code == null)
+    code = "" + this;
   
-  code=code.substring(code.indexOf("function")+8,code.indexOf("("));
+  code = code.substring(code.indexOf("function") + 8, code.indexOf("("));
   
   return code;//._trim();
 };
@@ -84,12 +84,12 @@ Function.prototype._name=function(code)
   @param code function code. Optional
   @return array of argument names
 */
-Function.prototype._argNamesString=function(code)
+Function.prototype._argNamesString = function(code)
 {
-  if(code==null)
-    code=""+this;
+  if(code == null)
+    code = "" + this;
   
-  return code.substring(code.indexOf("(")+1,code.indexOf(")"));
+  return code.substring(code.indexOf("(") + 1, code.indexOf(")"));
 };
 
 /**
@@ -97,31 +97,36 @@ Function.prototype._argNamesString=function(code)
   @param code function code. Optional
   @return array of argument names
 */
-Function.prototype._argNames=function(code)
+Function.prototype._argNames = function(code)
 {
-  code=this._argNamesString(code);
+  code = this._argNamesString(code);
   
-  return code.length>0 ? code.split(",") : [];
+  return (code.length !== 0) ? code.split(",") : new Array();
 };
 
 /**
-  @fn get function body. 'function name (a,b,c) {body}'
+  @fn get function body. 'function name (a, b, c) {body}'
   @param code function code. Optional
   @return body string
 */
-Function.prototype._body=function(code)
+Function.prototype._body = function(code)
 {
-  if(code==null)
+  if(code == null)
     code=""+this;
   
   return code.substring(code.indexOf("{"));
+};
+
+Function.prototype._isNative = function()
+{
+  return ('' + this).indexOf('[native code]') > -1;
 };
 
 /**
   @fn template function. do nothing. return 'this'
   @return 'this'
 */
-$jb._this=function()
+$jb._this = function()
 {
   return this;
 };
@@ -130,7 +135,7 @@ $jb._this=function()
   @fn template function. do nothing. return first argument
   @return 'arguments[0]'
 */
-$jb._self=function(v)
+$jb._self = function(v)
 {
   return v;
 };
@@ -139,66 +144,66 @@ $jb._self=function(v)
   @fn template function. do nothing. return null
   @return null
 */
-$jb._null=function()
+$jb._null = function()
 {
-}
+};
 
 /**
   @fn template function. do nothing. return 'true'
   @return 'true'
 */
-$jb._true=function()
+$jb._true = function()
 {
   return true;
-}
+};
 
 /**
   @fn template function. do nothing. return 'false'
   @return 'false'
 */
-$jb._false=function()
+$jb._false = function()
 {
   return false;
-}
+};
 
 /**
   @fn template function. do nothing. return '""'
   @return '""'
 */
-$jb._emptyString=function()
+$jb._emptyString = function()
 {
   return "";
 };
 
 (function()
 {
-  var _f=function _self()
+  var _fn = function _self()
   {
-    Function.canFastSelf=(_f==_self);
+    Function.canFastSelf = (_fn === _self);
   };
   
-  _f();
+  _fn();
 })();
 
 /**
   @fn create copyable function that return 'c'
   @return function. 'function.c' is 'c'
 */
-$jb._fConstC=function(c)
+$jb._fConstC = function(c)
 {
-  var __fRet=arguments.callee.__fRet;
-  var _ret=__fRet();
+  var __fRet = arguments.callee.__fRet,
+    _ret = __fRet();
   
-  _ret.c=c;
-  _ret.jbCopyRule=2;
-  _ret._copySelf=__fRet;
+  _ret.c = c;
+  _ret.jbCopyRule = 2;
+  _ret._copySelf = __fRet;
   
   return _ret;
 };
 
 if(Function.canFastSelf)
 {
-  $jb._fConstC.__fRet=function()
+  $jb._fConstC.__fRet = function()
   {
     return function _self()
     {
@@ -208,7 +213,7 @@ if(Function.canFastSelf)
 }
 else
 {
-  $jb._fConstC.__fRet=function()
+  $jb._fConstC.__fRet = function()
   {
     return function()
     {
@@ -221,7 +226,7 @@ else
   @fn create function that return 'c'
   @return function.
 */
-$jb._fConst=function(c)
+$jb._fConst = function(c)
 {
   return function()
   {
@@ -235,40 +240,40 @@ $jb._fConst=function(c)
   @param args array of arguments or null
   @return function. 'function.that' is 'that', 'function.args' is 'args' 
 */
-Function.prototype._fBindC=function(that,args)
+Function.prototype._fBindC = function(that, args)
 {
-  var __fRet=arguments.callee.__fRet;
-  var _ret=__fRet();
+  var __fRet = arguments.callee.__fRet,
+    _ret = __fRet();
 
-  _ret._func=this;
-  _ret.args=args;
-  _ret.that=that;
-  _ret.prototype=this.prototype;
-  _ret.jbCopyRule=2;
-  _ret._copySelf=__fRet;
+  _ret._fn = this;
+  _ret.args = args;
+  _ret.that = that;
+  _ret.prototype = this.prototype;
+  _ret.jbCopyRule = 2;
+  _ret._copySelf = __fRet;
 
   return _ret;
 };
 
 if(Function.canFastSelf)
 {
-  Function.prototype._fBindC.__fRet=function()
+  Function.prototype._fBindC.__fRet = function()
   {
     return function _self()
     {
-      return _self._func.apply(_self.that || this, _self.args || arguments);
+      return _self._fn.apply(_self.that || this, _self.args || arguments);
     };
   };
 }
 else
 {
-  Function.prototype._fBindC.__fRet=function()
+  Function.prototype._fBindC.__fRet = function()
   {
     return function()
     {
-      var _self=arguments.callee;
+      var _self = arguments.callee;
       
-      return _self._func.apply(_self.that || this, _self.args || arguments);
+      return _self._fn.apply(_self.that || this, _self.args || arguments);
     };
   };
 }
@@ -279,59 +284,95 @@ else
   @param args array of arguments or null
   @return function 
 */
-Function.prototype._fBind=function(that,args)
+if(Function.prototype.bind && Function.prototype.bind._isNative())
 {
-  var _func=this;
-  var _ret;
-  
-  var n=(that!=null)+((args!=null)<<1);
-  
-  if(n === 3)
-    _ret=function(){ return _func.apply(that, args); };
-  else if(n === 1)
-    _ret=function(){ return _func.apply(that, arguments); };
-  else if(n === 2)
-    _ret=function(){ return _func.apply(this, args); };
-  else
-    _ret=function(){ return _func.apply(this, arguments); };
-  
-  _ret.prototype=_func.prototype;
-  
-  return _ret;
-};
+  (function(){
+    var _bind  = Function.prototype.bind;
+    
+    Function.prototype._fBind = function(that, args)
+    {
+      var _fn = this, _ret;
+      
+      switch((that != null) + ((args != null) << 1))
+      {
+        case 3:
+          _ret = function(){ return _fn.apply(that, args); };
+          break;
+        case 1:
+          _ret = _bind.call(_fn, that);
+          break;
+        case 2:
+          _ret = function(){ return _fn.apply(this, args); };
+          break;
+        default:
+          _ret = function(){ return (arguments.length > 0) ? _fn.apply(this, arguments) : _fn.call(this); };
+      }
+      
+      _ret.prototype = _fn.prototype;
+      
+      return _ret;
+    };
+  })();  
+}
+else
+{
+  Function.prototype._fBind = function(that, args)
+  {
+    var _fn = this, _ret;
+    
+    switch((that != null) + ((args != null) << 1))
+    {
+      case 3:
+        _ret = function(){ return _fn.apply(that, args); };
+        break;
+      case 1:
+        _ret = function(){ return (arguments.length > 0) ? _fn.apply(that, arguments) : _fn.call(that); };
+        break;
+      case 2:
+        _ret = function(){ return _fn.apply(this, args); };
+        break;
+      default:
+        _ret = function(){ return (arguments.length > 0) ? _fn.apply(this, arguments) : _fn.call(this); };
+    }
+    
+    _ret.prototype = _fn.prototype;
+    
+    return _ret;
+  };
+}
 
 /**
   @fn create copyable function that create new class instance by target constructor
-  @return function. 'function._func' is target function 
+  @return function. 'function._fn' is target function 
 */
-Function.prototype._fNewC=function()
+Function.prototype._fNewC = function()
 {
-  var __fRet=arguments.callee.__fRet;
-  var _ret=__fRet();
+  var __fRet = arguments.callee.__fRet,
+    _ret = __fRet();
   
-  _ret._func=this;
-  _ret.jbCopyRule=2;
-  _ret._copySelf=__fRet;
+  _ret._fn = this;
+  _ret.jbCopyRule = 2;
+  _ret._copySelf = __fRet;
   
   return _ret;
 };
 if(Function.canFastSelf)
 {
-  Function.prototype._fNewC.__fRet=function()
+  Function.prototype._fNewC.__fRet = function()
   {
     return function _self()
     {
-      return new _self._func();
+      return new _self._fn();
     };
   };
 }
 else
 {
-  Function.prototype._fNewC.__fRet=function()
+  Function.prototype._fNewC.__fRet = function()
   {
     return function()
     {
-      return new arguments.callee._func();
+      return new arguments.callee._fn();
     };
   };
 }
@@ -340,70 +381,69 @@ else
   @fn create function that create new class instance by target constructor
   @return function 
 */
-Function.prototype._fNew=function()
+Function.prototype._fNew = function()
 {
-  var _func=this;
+  var _fn = this;
   
   return function()
   {
-    return new _func();
+    return new _fn();
   };
 };
 
 /**
   @fn create copyable function that wrap target function by '_wrapper' eg '_wrapper'(target function())
-  @return function. 'function._func' is target function, 'function._wrapper' is '_wrapper' 
+  @return function. 'function._fn' is target function, 'function._wrapper' is '_wrapper' 
 */
-Function.prototype._fWrapC=function(_wrapper)
+Function.prototype._fWrapC = function(_wrapper)
 {
-  var __fRet=arguments.callee.__fRet;
-  var _ret=__fRet();
+  var __fRet = arguments.callee.__fRet,
+    _ret = __fRet();
   
-  _ret._wrapper=_wrapper;
-  _ret.prototype=_wrapper.prototype;
-  _ret._func=this;
-  _ret.jbCopyRule=2;
-  _ret._copySelf=__fRet;
+  _ret._wrapper = _wrapper;
+  _ret.prototype = _wrapper.prototype;
+  _ret._fn = this;
+  _ret.jbCopyRule = 2;
+  _ret._copySelf = __fRet;
 
   return _ret;
 };
 if(Function.canFastSelf)
 {
-  Function.prototype._fWrapC.__fRet=function()
+  Function.prototype._fWrapC.__fRet = function()
   {
     return function _self()
     {
-      return _self._wrapper(_self._func.apply(this,arguments));
+      return _self._wrapper((arguments.length > 0) ? _self._fn.apply(this, arguments) : _self._fn.call(this));
     };
   };
 }
 else
 {
-  Function.prototype._fWrapC.__fRet=function()
+  Function.prototype._fWrapC.__fRet = function()
   {
     return function()
     {
-      var _self=arguments.callee;
+      var _self = arguments.callee;
       
-      return _self._wrapper(_self._func.apply(this,arguments));
+      return _self._wrapper((arguments.length > 0) ? _self._fn.apply(this, arguments) : _self._fn.call(this));
     };
   };
 }
 
 /**
   @fn create function that wrap target function by '_wrapper' eg '_wrapper'(target function())
-  @return function. 'function._func' is target function, 'function._wrapper' is '_wrapper' 
+  @return function. 'function._fn' is target function, 'function._wrapper' is '_wrapper' 
 */
-Function.prototype._fWrap=function(_wrapper)
+Function.prototype._fWrap = function(_wrapper)
 {
-  var _func=this;
+  var _fn = this,
+    _ret = function()
+    {
+      return _wrapper((arguments.length > 0) ? _fn.apply(this, arguments) : _fn.call(this));
+    };
   
-  var _ret=function()
-  {
-    return _wrapper(_func.apply(this,arguments));
-  };
-  
-  _ret.prototype=_wrapper.prototype;
+  _ret.prototype = _wrapper.prototype;
   
   return _ret;
 };
@@ -413,20 +453,21 @@ Function.prototype._fWrap=function(_wrapper)
   @param classes array of constructors
   @return result object 
 */
-$jb._new=function(classes)
+$jb._new = function(classes)
 {
-  var len=classes.length;
+  var len = classes.length;
   
-  if(len==0)
+  if(len === 0)
     return null;
-  if(len==1)
+  
+  if(len === 1)
     return new classes[0]();
     
-  var i=len;
-  var obj=new classes[--i]();
+  var i = len;
+  var obj = new classes[--i]();
   
   while(i--)
-    obj=new classes[i]($jb._fConst(obj));
+    obj = new classes[i]($jb._fConst(obj));
   
   return obj;
 };
@@ -436,12 +477,12 @@ $jb._new=function(classes)
   @param time time to delay. Optional. Default 0
   @return numeric id for pass to $w.clearTimeout to prevent execution
 */
-Function.prototype._defer=
-Function.prototype._delay=
-Function.prototype._after=
+Function.prototype._defer = 
+Function.prototype._delay = 
+Function.prototype._after = 
 function(time)
 {
-  return $w.setTimeout(this,time || 0);
+  return setTimeout(this, time || 0);
 };
 
 /**
@@ -449,9 +490,9 @@ function(time)
   @param time time to delay between execution.
   @return numeric id for pass to $w.clearInterval to prevent period execution
 */
-Function.prototype._period=function(time)
+Function.prototype._period = function(time)
 {
-  return $w.setInterval(this,time);
+  return setInterval(this, time);
 };
 
 /**
@@ -461,9 +502,9 @@ Function.prototype._period=function(time)
   @param args 'arguments' for eval
   @return result of eval
 */
-$jb._funcEval=function(str,that,args)
+$jb._funcEval = function(str, that, args)
 {
-  return (new Function("",str))._apply(that,args);
+  return (new Function("", str))._apply(that, args);
 };
 
 /**
@@ -473,21 +514,21 @@ $jb._funcEval=function(str,that,args)
   @param args 'arguments' for eval
   @return result of eval
 */
-$jb._funcEvalSafe=function(str,that,args)
+$jb._funcEvalSafe = function(str, that, args)
 {
-  if(str=="")
+  if(str === "")
     return null;
     
-  var ret=null;
+  var ret = null;
   
   try
   {
-    ret=(new Function("",str))._apply(that,args);
+    ret=(new Function("", str))._apply(that, args);
   }
   catch(err)
   {
     //($jb._error || $G.alert)(err.message);
-    ret=null;
+    ret = null;
   }
   
   return ret;
@@ -498,11 +539,11 @@ $jb._funcEvalSafe=function(str,that,args)
   @param str string to eval
   @return function that eval 'str'
 */
-$jb._fEval=function(str)
+$jb._fEval = function(str)
 {
   return function()
   {
-    return $G.eval(str);
+    return eval(str);
   };
 };
 
