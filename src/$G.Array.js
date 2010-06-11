f6901c7a5a748a07e7b9f27adef205b92a53e6f2
bga <bga.email@gmail.com>
2010-06-11 18:01:26 +0400

    new Array.prototype._uniqueSelf; loop optimization for opera improved; loop unrolling for copy operation(speed up); optimization map not do not uses yet

--- src/$G.Array.js
+++ src/$G.Array.js
@@ -123,61 +123,342 @@ if(typeof([].lastIndexOf) !== "function")
   };
 }
 
-/*
-if($w.opera)
+Array.prototype._uniqueSelf = function()
 {
-  $temp.arrayParamMap.forwardLoop = "for(i = 0; i < len; i++)";
-  $temp.arrayParamMap.backwardLoop = "for(i = len; i >= 0 ; i--)";
+  var len = this.length, i = 0;
+
+  while (i < len && this[i] !== this[++i])
+    ;
+
+  if(i >= len)
+    return this;
+    
+  var j = --i, v;
+   
+  while(i < len)
+  {
+    v = this[j++] = this[i];  
+    
+    while (++i < len && v === this[i])
+      ;
+  }
+  
+  this.length = j;
+
+  return this;
+};
+
+
+(function(p)
+{
+
+if($d.recalc)
+{
+  p.
+  _define('VAR_DECLARE',
+    "var v;"
+  ).  
+  _define('VALUE_TEST',
+    "typeof(v=this[i])!=='undefined'"
+  ).  
+  _define('VALUE_TEST_NOT',
+    "typeof(v=this[i])==='undefined'"
+  ).  
+  _define('VALUE_PASS',
+    "v"
+  );  
 }
 else
 {
-  $temp.arrayParamMap.forwardLoop = "i = -1; while(++i < len)";
-  $temp.arrayParamMap.backwardLoop = "i = len; while(i--)";
+  p.
+  _define('VAR_DECLARE',
+    ""
+  ).  
+  _define('VALUE_TEST',
+    "(i in this)"
+  ).  
+  _define('VALUE_TEST_NOT',
+    "!(i in this)"
+  ).  
+  _define('VALUE_PASS',
+    "this[i]"
+  );  
 }
+
+if($w.opera)
+{
+  p.
+  /*
+  _define('_D_LOOP_BODY($B_$, $E_$)',
+    "for(;$B_$<$E_$;$B_$++)"
+  ).  
+  _define('_D_LOOP_INIT($B_$)',
+    "$B_$=0;"
+  ).  
+  _define('_D_LOOP_NORM($B_$)',
+    "++$B_$;"
+  ).
+  */
+  _define('_D_LOOP_BODY($B_$, $E_$)',
+    "while(++$B_$<$E_$)"
+  ).  
+  _define('_D_LOOP_INIT($B_$)',
+    "$B_$=-1;"
+  ).  
+  _define('_D_LOOP_NORM($B_$)',
+    ""
+  ).  
+  _define('_I_LOOP_BODY($B_$)',
+    "for(;$B_$>=0;$B_$--)"
+  ).  
+  _define('_I_LOOP_INIT($B_$, $E_$)',
+    "$B_$=$E_$-1;"
+  ).  
+  _define('_I_LOOP_NORM($B_$)',
+    "--$B_$;"
+  );  
+}
+else
+{
+  p.
+  _define('_D_LOOP_BODY($B_$, $E_$)',
+    "while(++$B_$<$E_$)"
+  ).  
+  _define('_D_LOOP_INIT($B_$)',
+    "$B_$=-1;"
+  ).  
+  _define('_D_LOOP_NORM($B_$)',
+    ""
+  ).  
+  _define('_I_LOOP_BODY($B_$)',
+    "while($B_$--)"
+  ).  
+  _define('_I_LOOP_INIT($B_$, $E_$)',
+    "$B_$=$E_$;"
+  ).  
+  _define('_I_LOOP_NORM($B_$)',
+    ""
+  );  
+}
+
+p.
+_define('_I_LOOP_FULL($B_$, $E_$)',
+  '_I_LOOP_INIT($B_$,$E_$) _I_LOOP_BODY($B_$)'
+).
+_define('_D_LOOP_FULL($B_$, $E_$)',
+  '_D_LOOP_INIT($B_$) _D_LOOP_BODY($B_$, $E_$)'
+);  
+
+p._define('_I_COPY($SRC, $DEST, $SRC_END, $DEST_END, $I, $J, $LOOP_COND_EQ_INDEXES, $LOOP_COND, $MOD8, $MOD8SUB8)',
+$jb._preprocessingTextBegin(function(){
+  if($DEST_END === $SRC_END)
+  {
+    /*
+    $J = $DEST_END - 8; 
+    
+    while($J >= 0)
+    //for(; j >= 0; j -= 8, i -= 8)
+    {  
+      $DEST[$J    ] = $SRC[$J    ];
+      $DEST[$J + 1] = $SRC[$J + 1];
+      $DEST[$J + 2] = $SRC[$J + 2];
+      $DEST[$J + 3] = $SRC[$J + 3];
+      $DEST[$J + 4] = $SRC[$J + 4];
+      $DEST[$J + 5] = $SRC[$J + 5];
+      $DEST[$J + 6] = $SRC[$J + 6];
+      $DEST[$J + 7] = $SRC[$J + 7];
+      $J -= 8;
+    }
+    
+    switch($J)
+    {
+      case -1: $DEST[$J + 1] = $SRC[$J + 1]; 
+      case -2: $DEST[$J + 2] = $SRC[$J + 2]; 
+      case -3: $DEST[$J + 3] = $SRC[$J + 3]; 
+      case -4: $DEST[$J + 4] = $SRC[$J + 4]; 
+      case -5: $DEST[$J + 5] = $SRC[$J + 5]; 
+      case -6: $DEST[$J + 6] = $SRC[$J + 6]; 
+      case -7: $DEST[$J + 7] = $SRC[$J + 7]; 
+    }
+    */
+    
+    $I = $DEST_END&7; $J = $DEST_END - $I; 
+    
+    while($LOOP_COND_EQ_INDEXES)
+    //for(; j >= 0; j -= 8, i -= 8)
+    {  
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+      $DEST[--$J] = $SRC[$J];
+    }
+    
+    switch($MOD8)
+    {
+      case 7: $DEST[--$I] = $SRC[$I]; 
+      case 6: $DEST[--$I] = $SRC[$I]; 
+      case 5: $DEST[--$I] = $SRC[$I]; 
+      case 4: $DEST[--$I] = $SRC[$I]; 
+      case 3: $DEST[--$I] = $SRC[$I]; 
+      case 2: $DEST[--$I] = $SRC[$I]; 
+      case 1: $DEST[--$I] = $SRC[$I]; 
+    }
+    
+  }
+  else
+  {
+    $J = $DEST_END - 8; $I = $SRC_END - 8; 
+    
+    while($LOOP_COND)
+    //for(; j >= 0; j -= 8, i -= 8)
+    {  
+      $DEST[$J    ] = $SRC[$I    ];
+      $DEST[$J + 1] = $SRC[$I + 1];
+      $DEST[$J + 2] = $SRC[$I + 2];
+      $DEST[$J + 3] = $SRC[$I + 3];
+      $DEST[$J + 4] = $SRC[$I + 4];
+      $DEST[$J + 5] = $SRC[$I + 5];
+      $DEST[$J + 6] = $SRC[$I + 6];
+      $DEST[$J + 7] = $SRC[$I + 7];
+      $J -= 8; $I -= 8;
+    }
+    
+    switch($MOD8SUB8)
+    {
+      case -1: $DEST[$J + 1] = $SRC[$I + 1]; 
+      case -2: $DEST[$J + 2] = $SRC[$I + 2]; 
+      case -3: $DEST[$J + 3] = $SRC[$I + 3]; 
+      case -4: $DEST[$J + 4] = $SRC[$I + 4]; 
+      case -5: $DEST[$J + 5] = $SRC[$I + 5]; 
+      case -6: $DEST[$J + 6] = $SRC[$I + 6]; 
+      case -7: $DEST[$J + 7] = $SRC[$I + 7]; 
+    }
+  }  
+
+})._preprocessingTextEnd()
+);
+/*
+_I_LOOP_NORM($I);
+_I_LOOP_FULL($I)
+  $DEST[$I] = $SRC[--$J];
 */
 
-eval((new $jb.Preprocessor()).
-_define('VAR_DECLARE',
-  ($d.recalc) ? 'var v;' : ''
-).  
-_define('VALUE_TEST',
-  ($d.recalc) ? "typeof(v=this[i])!=='undefined'" : '(i in this)'
-).  
-_define('VALUE_TEST_NOT',
-  ($d.recalc) ? "typeof(v=this[i])==='undefined'" : "!(i in this)"
-).  
-_define('VALUE_PASS',
-  ($d.recalc) ? "v" : "this[i]"
-).  
-_define('T_LBL:',
-  ''
-).  
-_define('D_LOOP_BODY(B_, E_)',
-  ($w.opera) ? "for(;B_<E_;B_++)" : "while(++B_<E_)"
-).  
-_define('D_LOOP_INIT(B_)',
-  ($w.opera) ? "B_=0;" : "B_=-1;"
-).  
-_define('D_LOOP_NORM(B_)',
-  ($w.opera) ? "++B_;" : ""
-).  
-_define('D_LOOP_FULL(B_, E_)',
-  'D_LOOP_INIT(B_) D_LOOP_BODY(B_,E_)'
-).  
-_define('I_LOOP_BODY(B_)',
-  ($w.opera) ? "for(;B_>=0;B_--)" : "while(B_--)"
-).  
-_define('I_LOOP_INIT(B_, E_)',
-  ($w.opera) ? "B_=E_-1;" : "B_=E_;"
-).  
-_define('I_LOOP_NORM(B_)',
-  ($w.opera) ? "--B_;" : ""
-).  
-_define('I_LOOP_FULL(B_, E_)',
-  'I_LOOP_INIT(B_,E_) I_LOOP_BODY(B_)'
-).
-_pass(
+//(new Function('', p._pass(
+//$G._log(p._pass(
+eval(p._pass(
 $jb._preprocessingTextBegin(function(){
+
+/*
+Array.prototype._reverseCopy = function()
+{
+  var i = this.length, a = new Array(i), j = 0;
+  var mod8 = i&7;
+  
+  i -= mod8;
+  j += mod8;
+  
+  while(i)
+  {
+    a[j    ] = this[i    ];
+    a[j + 1] = this[i - 1];
+    a[j + 2] = this[i - 2];
+    a[j + 3] = this[i - 3];
+    a[j + 4] = this[i - 4];
+    a[j + 5] = this[i - 5];
+    a[j + 6] = this[i - 6];
+    a[j + 7] = this[i - 7];
+    j += 8; i -= 8;
+  }
+  
+  switch(mod8)
+  {
+    case 7: a[0] = this[j    ];
+    case 6: a[1] = this[j - 1];
+    case 5: a[2] = this[j - 2];
+    case 4: a[3] = this[j - 3];
+    case 3: a[4] = this[j - 4];
+    case 2: a[5] = this[j - 5];
+    case 1: a[6] = this[j - 6];
+  }
+  
+  return a;  
+};
+
+if(!([].splice) || ($w.opera && $w.opera.version() < 11.0))
+{
+  // opera has 4x slower code .slice realisation that clear js realisation
+  Array.prototype.splice = function(begin, howMany)
+  {
+    var len = this.length;
+    
+    if(begin == null)
+    {
+      begin = 0;
+    }
+    else if(begin < 0)
+    {
+      begin += len;
+     
+      if(begin < 0)
+        begin = 0;
+    }
+    else if(begin >= len)
+    {
+      begin = len;
+      howMany = 0;
+    }
+    
+    var ret, i, j, k, aLen = arguments.length, aLenSub2 = aLen - 2;
+    
+    if(howMany > 0)
+    {
+      ret = new Array(howMany);
+      j = begin + howMany; 
+      
+      if(aLenSub2 === howMany)
+      {
+        k = aLen;
+        
+        _I_LOOP_FULL(i, howMany)
+          ret[i] = this[--j], this[j] = arguments[--k];
+
+        return ret;
+      }
+      else
+      {
+        _I_LOOP_FULL(i, howMany)
+          ret[i] = this[--j];
+      }
+    }
+    else
+    {
+      ret = new Array();
+    }
+    
+    if(arguments.length > 0)
+    {
+      j = arguments.length, i = begin + j - 2;
+      
+      while(--j >= 2)
+        this[--i] = arguments[j];
+    }
+    
+    if(howMany > 0)
+    {
+      i = len, j = len - howMany, k = begin + arguments.length - 2;
+      
+      while(--j >= k)
+        this[j] = this[--i];
+    }
+    
+    return ret;  
+  };
+}
+*/
 
 if(!([].slice) || ($w.opera && $w.opera.version() < 11.0))
 {
@@ -221,18 +502,55 @@ if(!([].slice) || ($w.opera && $w.opera.version() < 11.0))
     if((len = end - begin) <= 0)
       return [];
       
+    /*
     var a = new Array(len), i;
     
-    I_LOOP_FULL(i, len)
+    _I_LOOP_FULL(i, len)
       a[i] = this[--end];
-      
-    return a;  
+    */
+    
+    /*
+    var a = new Array(len), mod8 = len%8, j = len - mod8, i = end - mod8;
+    
+    while(j)
+    {  
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+      a[--j] = this[--i];
+    }
+    
+    j = len; i = end;
+    
+    switch(mod8)
+    {
+      case 7: a[--j] = this[--i]; 
+      case 6: a[--j] = this[--i]; 
+      case 5: a[--j] = this[--i]; 
+      case 4: a[--j] = this[--i]; 
+      case 2: a[--j] = this[--i]; 
+      case 3: a[--j] = this[--i]; 
+      case 1: a[--j] = this[--i]; 
+      case 1: a[--j] = this[--i]; 
+      case 0: a[--j] = this[--i]; 
+    }
+    */
+    
+    var a = new Array(len), j, i;
+
+    _I_COPY(this, a, end, len, i, j, j, j >= 0, i, j);
+    
+    return a;
   };
 }
 
 if($w.opera || ![].concat)
 {
-  Array.prototype.concat=function()
+  Array.prototype.concat = function()
   {
     var thisLen = this.length, 
     a = new Array(thisLen), j, 
@@ -249,16 +567,18 @@ if($w.opera || ![].concat)
     var _isArray = Array.isArray;
     
     j = thisLen;
-    D_LOOP_FULL(i, argsLen)
+    _D_LOOP_FULL(i, argsLen)
     {
       if(_isArray(b = arguments[i]))
       {
         if((bLen = b.length) > 0)
         {
-          j = l = a.length += bLen;
+          j = a.length += bLen;
           
-          I_LOOP_FULL(k, bLen)
+          _I_COPY(b, a, bLen, j, k, l, k, k > 8, l, k);
+          /*_I_LOOP_FULL(k, bLen)
             a[--l] = b[k];
+          */
         }    
       }
       else
@@ -282,7 +602,7 @@ if(typeof([].filter) !== "function")
         throw new TypeError();
       
       var i = -1, len = this.length, j = 0,
-        a = new Array(), v;
+        a = [], v;
       
       if(that == null)
       {
@@ -316,16 +636,16 @@ if(typeof([].filter) !== "function")
       
       if(that == null)
       {
-        D_LOOP_FULL(i, len)
-        T_LBL:{
+        _D_LOOP_FULL(i, len)
+        {
           if((i in this) && _fn((v = this[i]), i, this))
             a[j++] = v;
         }  
       }
       else
       {
-        D_LOOP_FULL(i, len)
-        T_LBL:{
+        _D_LOOP_FULL(i, len)
+        {
           if((i in this) && _fn.call(that, (v = this[i]), i, this))
             a[j++] = v;
         }    
@@ -350,7 +670,7 @@ if(typeof([].forEach) !== "function")
     
     if(that == null)
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST)
           _fn(VALUE_PASS, i, this);
@@ -358,7 +678,7 @@ if(typeof([].forEach) !== "function")
     }
     else
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST)
           _fn.call(that, VALUE_PASS, i, this);
@@ -381,7 +701,7 @@ if(typeof([].every) !== "function")
 
     if(that == null)
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST && !_fn(VALUE_PASS, i, this))
           return false;
@@ -389,7 +709,7 @@ if(typeof([].every) !== "function")
     }
     else
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST && !_fn.call(that, VALUE_PASS, i, this))
           return false;
@@ -415,7 +735,7 @@ if(typeof([].map) !== "function")
     
     if(that == null)
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST)
           a[i] = _fn(VALUE_PASS, i, this);
@@ -423,7 +743,7 @@ if(typeof([].map) !== "function")
     }
     else
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST)
           a[i] = _fn.call(that, VALUE_PASS, i, this);
@@ -448,7 +768,7 @@ if(typeof([].some) !== "function")
 
     if(that == null)
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST && _fn(VALUE_PASS, i, this))
           return true;
@@ -456,7 +776,7 @@ if(typeof([].some) !== "function")
     }
     else
     {
-      D_LOOP_FULL(i, len)
+      _D_LOOP_FULL(i, len)
       {
         if(VALUE_TEST && _fn.call(that, VALUE_PASS, i, this))
           return true;
@@ -495,8 +815,8 @@ if(typeof([].reduce) !== "function")
 
     VAR_DECLARE;
     
-    D_LOOP_NORM(i)
-    D_LOOP_BODY(i, len)
+    _D_LOOP_NORM(i)
+    _D_LOOP_BODY(i, len)
     {  
       if(VALUE_TEST)  
         rv = _fn(rv, VALUE_PASS, i, this);  
@@ -532,8 +852,8 @@ if(typeof([].reduceRight) !== "function")
 
     VAR_DECLARE;
 
-    I_LOOP_NORM(i)
-    I_LOOP_BODY(i)
+    _I_LOOP_NORM(i)
+    _I_LOOP_BODY(i)
     {
       if(VALUE_TEST)
         rv = _fn(rv, VALUE_PASS, i, this);
@@ -543,6 +863,9 @@ if(typeof([].reduceRight) !== "function")
   };
 }
 })._preprocessingTextEnd()
-));
+) // _pass
+//) // new Function
+);
+})(new $jb.Preprocessor());
 
 });
