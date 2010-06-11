80a19bdead4a4139d64288d3ba451a72f30249db
bga <bga.email@gmail.com>
2010-06-11 19:16:21 +0400

    better code minification; global caching;

--- src/$jb.nav.js
+++ src/$jb.nav.js
@@ -39,123 +39,128 @@
 
 $jb.Loader._scope().
 _willDeclared("$jb/$jb.nav.js").
-_completed(function(){
+_completed(function($G, $jb){
+
+var $w = $G.$w, navigator = $w.navigator, RegExp = $G.RegExp;
 
 /**
   @class detector of user agent class and traits
   @brief version is a positive float number(9.5) if can detect, -1 if feature present but version can not detected, else null
 */
-$jb.Nav=function()
+$jb.Nav = function()
 {
   /** @var Internet Exploror version. Detected in constructor by this.__detectUserAgent */
-  this.ie_=null;
+  this.ie_;
   
   /** @var Opera version. Detected in constructor by this.__detectUserAgent  */
-  this.opera_=null;
+  this.opera_;
 
   /** @var Mozilla Firefox version. Detected in constructor by this.__detectUserAgent  */
-  this.ff_=null;
+  this.ff_;
 
   /** @var WebKit version. Detected in constructor by this.__detectUserAgent  */
-  this.webkit_=null;
+  this.webkit_;
   
   /** @var Adobe AIR version. Detected in constructor by this.__detectUserAgent  */
-  this.air_=null;
+  this.air_;
 
   /** @var User Agent Name string */
-  this.uaName_=null;
+  this.uaName_;
   
   /** @var Unique User Agent Id string that contains name, version, build number ... separeted by tab. Usefull for userAgent specific optimisation coeff selection and generation */
-  this.uaId_=null;
+  this.uaId_;
   
   /** @var Native XPath version. Detected in constructor */
-  this.nativeXPath_= (document.evaluate!=null) ? -1 : null;
+  this.nativeXPath_ = ('evaluate' in $d) ? -1 : null;
   
   /** @var Adobe Flash plugin version. Detected on demand. */
-  this.flash_=null;
+  this.flash_;
   
   this.__detectUserAgent();
 };  
+
+/** @alias */
+var NavProto = $jb.Nav.prototype;
 
 /**
   @fn get version of Internet Exploror
   @return version
 */
-$jb.Nav.prototype._ie=function() { return this.ie_; };
+NavProto._ie = function() { return this.ie_; };
 
 /**
   @fn get version of Opera
   @return version
 */
-$jb.Nav.prototype._opera=function() { return this.opera_; };
+NavProto._opera = function() { return this.opera_; };
 
 /**
   @fn get version of Mozilla Firefox
   @return version
 */
-$jb.Nav.prototype._ff=function() { return this.ff_; };
+NavProto._ff = function() { return this.ff_; };
 
 /**
   @fn get version of WebKit
   @return version
 */
-$jb.Nav.prototype._webkit=function() { return this.webkit_; };
+NavProto._webkit = function() { return this.webkit_; };
 
 /**
   @fn get version of Adobe AIR
   @return version
 */
-$jb.Nav.prototype._air=function() { return this.air_; };
+NavProto._air = function() { return this.air_; };
 
 /**
   @fn get version of Native XPath
   @return version
 */
-$jb.Nav.prototype._nativeXPath=function() { return this.nativeXPath_; };
+NavProto._nativeXPath = function() { return this.nativeXPath_; };
 
 /**
   @fn get version of XPath
   @return version
 */
-$jb.Nav.prototype._xpath=function() { return (document.evaluate!=null) ? -1 : null; };
+NavProto._xpath = function() { return ($d.evaluate != null) ? -1 : null; };
 
 /**
   @fn get version of Adobe Flash plugin
   @return version
 */
-$jb.Nav.prototype._flash=function()
+NavProto._flash = function()
 {
   if(this.flash_)
     return this.flash_;
     
-  if(this._ie() && window.ActiveXObject!=null)
+  if(this._ie() && $w.ActiveXObject != null)
   {
-    var sw="ShockwaveFlash.ShockwaveFlash";
+    var sw = 'ShockwaveFlash.ShockwaveFlash';
     
     try
     {
-      var axo=new ActiveXObject(sw);
-      var ver=axo.GetVariable("$version")+"";
+      var axo = new ActiveXObject(sw),
+        ver = '' + axo.GetVariable('$version');
       
       if(/ (\d+\,\d+)/.test(ver))
-        this.flash_=($G.RegExp.$1+"").replace(",",".")-0;
+        this.flash_ = +RegExp.$1.replace(',', '.');
     }
     catch(err)
     {
-      this.flash_=null;
+      this.flash_ = null;
     }
   }
-  else if(navigator.plugins && navigator.plugins["Shockwave Flash"])
+  else if(navigator.plugins && navigator.plugins['Shockwave Flash'])
   {
-    var d=navigator.plugins["Shockwave Flash"].description;
-    var n=navigator,ms,m;
+    var n = navigator, d = n.plugins['Shockwave Flash'].description,
+       ms, m;
     
-    if(d!=null && !((ms=n.mimeTypes) && (m=ms["application/x-shockwave-flash"]) && !m.enabledPlugin))
+    if(d != null && !((ms = n.mimeTypes) && (m = ms['application/x-shockwave-flash']) && !m.enabledPlugin))
     {
-      d+="";
+      d += '';
       
       if(/ (\d+\.\d+)/.test(d))
-        this.flash_=$G.RegExp.$1-0;
+        this.flash_ = +RegExp.$1;
     }  
   }
   
@@ -165,46 +170,46 @@ $jb.Nav.prototype._flash=function()
 /**
   @fn detect user agent
 */
-$jb.Nav.prototype.__detectUserAgent=function()
+NavProto.__detectUserAgent = function()
 {
-  var t=$w.navigator.userAgent;
+  var t = $w.navigator.userAgent;
 
-  if (typeof($w.navigator.mozIsLocallyAvailable)=="function")
+  if (typeof($w.navigator.mozIsLocallyAvailable) === 'function')
   {
     if(/Firefox[\/\s](\d+\.\d+)/.test(t))
-      this.ff_=$G.RegExp.$1-0;
+      this.ff_ = +RegExp.$1;
     else
-      this.ff_=-1;
+      this.ff_ = -1;
       
-    this.uaId_=(this.uaName_="Firefox")+"\t"+this.ff_;  
+    this.uaId_ = (this.uaName_ = 'Firefox') + '\t' + this.ff_;  
   }
-  else if($w.opera && typeof($w.opera.version)=="function")
+  else if($w.opera && typeof($w.opera.version) === 'function')
   {
-    this.opera_=$w.opera.version()-0;
-    this.uaId_=(this.uaName_="Opera")+"\t"+this.opera_;  
+    this.opera_ = +$w.opera.version();
+    this.uaId_ = (this.uaName_ = 'Opera') + '\t' + this.opera_;  
   }
-  else if(typeof($w.WebKitPoint)=="function") /* for example */
+  else if(typeof($w.WebKitPoint) === 'function') /* for example */
   {
     if(/WebKit[\/\s](\d+\.\d+)/.test(t))
-      this.webkit_=$G.RegExp.$1-0;
+      this.webkit_ = +RegExp.$1;
     else
-      this.webkit_=-1;
+      this.webkit_ = -1;
 
-    this.uaId_=(this.uaName_="WebKit")+"\t"+this.webkit_;  
+    this.uaId_ = (this.uaName_ = 'WebKit') + '\t' + this.webkit_;  
   }
-  else if("recalc" in $d)
+  else if('recalc' in $d)
   {
     if(/MSIE (\d+\.\d+);/.test(t))
-      this.ie_=$G.RegExp.$1-0;
+      this.ie_ = +RegExp.$1;
     else
-      this.ie_=-1;
+      this.ie_ = -1;
 
-    this.uaId_=(this.uaName_="IE")+"\t"+this.ie_;  
+    this.uaId_ = (this.uaName_ = 'IE') + '\t' + this.ie_;  
   }
   
   if(/AdobeAIR\/(\d+\.\d+)/.test(t))
   {
-    this.air_=$G.RegExp.$1-0;
+    this.air_ = +RegExp.$1;
   }
 };
 
@@ -212,7 +217,7 @@ $jb.Nav.prototype.__detectUserAgent=function()
   @fn get User Agent Name string
   @return User Agent Name string
 */
-$jb.Nav.prototype._uaName=function()
+NavProto._uaName = function()
 {
   return this.uaName_;
 };
@@ -221,13 +226,13 @@ $jb.Nav.prototype._uaName=function()
   @fn get User Agent Id string
   @return User Agent Id string
 */
-$jb.Nav.prototype._uaId=function()
+NavProto._uaId = function()
 {
   return this.uaId_;
 };
 
 
 /** @var helper instance of $jb.Nav for all user agent specific scripts */
-$jb.nav=new $jb.Nav();
+$jb.nav = new $jb.Nav();
 
 });
