013ba8e06938622423110264d4cc9388a9fd2f85
bga <bga.email@gmail.com>
2010-06-30 22:37:40 +0400

    dajngo templates support

---
+++ src/$jb.Template.Django.js
@@ -0,0 +1,155 @@
+/**
+  @file
+  @author  Fyodorov "bga" Alexander <bga.email@gmail.com>
+ 
+  @section LICENSE
+ 
+  Experimental common javascript RIA library http://github.com/bga/jbasis
+
+  Copyright (c) 2009-2010, Fyodorov "Bga" Alexander <bga.email@gmail.com>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+      * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+      * The name of the developer may not be used to endorse or promote
+        products derived from this software without specific prior
+        written permission.
+
+  THIS SOFTWARE IS PROVIDED BY FYODOROV "BGA" ALEXANDER "AS IS" AND ANY EXPRESS OR
+  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+  IN NO EVENT SHALL FYODOROV "BGA" ALEXANDER BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+  @section DESCRIPTION
+*/
+
+$jb.Loader._scope().
+//_require("$jb/$G.Function.js").
+_require("$jb/$jb.nav.js").
+_require("$jb/$G.String.js").
+_willDeclared("$jb/$jb.Template.Django.js").
+_completed(function($G, $jb){
+
+if($jb.Template == null)
+  $jb.Template = {};
+
+$jb.Template.Django = function(text, argNames)
+{
+  if(text != null)
+    this._compile(text, argNames);  
+  
+  this.text;
+  this.argNames;
+  this.__compiled;
+};
+
+/** @alias */
+var TemplateDjangoProto = $jb.Template.Django.prototype;
+
+(function()
+{
+  var Function = $G.Function;
+  var
+    stringRE = /(\}\}|%\})([\s\S]*?)(\{\{|\{%)/g, 
+    exprRE = /\{\{([\s\S]*?)\}\}/g,
+    codeRE = /\{%([\s\S]*?)%\}/g
+    ;
+    
+  if($jb.nav._ie())
+  {
+    var _stringReplacer = function(all, begin, a, end)
+    {
+      return begin + '\'' + a._escapeForCString() + '\'//jb\n,' + end;
+    };
+    var extraConcatOPRE = /\/\/jb\n\,\);/g;
+    
+    TemplateDjangoProto._compile = function(text, argNames)
+    {
+      if(text == null)
+        text = this.text;
+        
+      if(text === this.text && this.__compiled)
+        return this;
+        
+      this.__compiled = new Function(argNames || '',
+      //$G._log(  
+        "var jbTmlTexts = [],\n\
+        _jbTmlJoin = Array.prototype.join,\n\
+        _echo = function(){ jbTmlTexts.push(_jbTmlJoin.call(arguments, ' ')); }\n\
+        jbTmlTexts.push(" + 
+        (
+          ('}}' + text + '{{').
+            replace(stringRE, _stringReplacer).  
+            replace(exprRE, '($1)//jb\n,').
+            replace(codeRE, ');$1;jbTmlTexts.push('). 
+            slice(2, -2) + 
+          ');return jbTmlTexts.join("");'
+        ).replace(extraConcatOPRE, ');')
+      );
+
+      this.text = text;
+      this.argNames = argNames;
+
+      return this;
+    };
+  }
+  else
+  {
+    var _stringReplacer = function(all, begin, a, end)
+    {
+      return begin + '\'' + a._escapeForCString() + '\'//jb\n+' + end;
+    };
+    var extraConcatOPRE = /\/\/jb\n\+;/g;
+    
+    TemplateDjangoProto._compile = function(text, argNames)
+    {
+      if(text == null)
+        text = this.text;
+        
+      if(text === this.text && this.__compiled)
+        return this;
+        
+      this.__compiled = new Function(argNames || '',
+      //$G._log(  
+        "var jbTmlText,\n\
+        _jbTmlJoin = Array.prototype.join,\n\
+        _echo = function(){ jbTmlText += _jbTmlJoin.call(arguments, ' '); }\n\
+        jbTmlText = " + 
+        (
+          ('}}' + text + '{{').
+            replace(stringRE, _stringReplacer).  
+            replace(exprRE, '($1)//jb\n+').
+            replace(codeRE, ';$1;jbTmlText+=').
+            slice(2, -2) + 
+          ';return jbTmlText;'
+        ).replace(extraConcatOPRE, ';')
+      );
+
+      this.text = text;
+      this.argNames = argNames;
+      
+      return this;
+    };
+  }  
+})();  
+
+TemplateDjangoProto._apply = function(that, args)
+{
+  if(typeof(this.__compiled) != 'function')
+    throw "template not compiled";
+  
+  return (args != null) ? this.__compiled.apply(that, args) : this.__compiled.call(that);
+};
+
+});
